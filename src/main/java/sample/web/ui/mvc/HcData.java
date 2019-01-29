598b194c676eced77cc1a9b70940377f26deaed4
yoojaehoon <jaebungbung@naver.com>
2019-01-29 10:48:30 +0900

    initial commit of springboot-web-ui-healthcheck

---
+++ src/main/java/sample/web/ui/mvc/HcData.java
@@ -0,0 +1,24 @@
+package sample.web.ui.mvc;
+public class HcData {
+
+    String id;
+    String content;
+
+    public HcData() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+    public void setContent(String content) {
+	this.content = content;
+    }
+}
